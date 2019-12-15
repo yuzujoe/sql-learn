cf354cc9c96276bd9870a613e7b4241f5cfd1ad1
yuzujoe <yuzun225@gmail.com>
2019-12-15 14:06:06 +0900

    paractice-1

---
+++ practice-1/lesson-1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Jyushoroku
+(
+  toroku_bango INTEGER NOT NULL,
+  namea VARCHAR(128) NOT NULL,
+  jyusyo VARCHAR(128) NOT NULL,
+  tel_no CHAR(10),
+  mail_address CHAR(120),
+  PRIMARY KEY (toroku_bango)
+);
---
+++ practice-1/lesson-2.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Jyushoroku ADD COLUMN yubin_bango CHAR(8) NOT NULL;
