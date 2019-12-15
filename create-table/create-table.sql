d11b5163d956ac314142451346c550b7d71dac34
yuzujoe <yuzun225@gmail.com>
2019-12-15 11:45:31 +0900

    create-table

---
+++ create-table/create-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE shohin
+(
+  shohin_id CHAR(4) NOT NULL,
+  shohin_mei VARCHAR(100) NOT NULL,
+  shohin_bunrui VARCHAR(32) NOT NULL,
+  hanbai_tanka INTEGER,
+  shiire_tanka INTEGER,
+  torokubi DATE,
+  PRIMARY KEY (shohin_id)
+);
