0830d569c6068e55c28ff6fa333f674af08176b5
yuzujoe <yuzun225@gmail.com>
2019-12-19 08:21:07 +0900

    select as

--- select/select.sql
+++ select/select.sql
@@ -1,3 +1,7 @@
 SELECT shohin_id, shohin_mei, shiire_tanka FROM shohin;
 -- 全件検索
 SELECT * from shohin;
+-- ASを使用して別名をつけることもできる
+SELECT shohin_id as id, shohin_mei as namae, shiire_tanka as tanka FROM shohin;
+-- ""で囲むと日本語でも表示される
+SELECT shohin_id as "商品ID", shohin_mei as "商品名", shiire_tanka as "仕入単価" FROM shohin;
