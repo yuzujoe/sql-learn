304b36941757bf4d10304390b97531dda5221360
yuzujoe <yuzun225@gmail.com>
2019-12-25 23:23:22 +0900

    distinct

--- select/select.sql
+++ select/select.sql
@@ -5,3 +5,9 @@ SELECT * from shohin;
 SELECT shohin_id as id, shohin_mei as namae, shiire_tanka as tanka FROM shohin;
 -- ""で囲むと日本語でも表示される
 SELECT shohin_id as "商品ID", shohin_mei as "商品名", shiire_tanka as "仕入単価" FROM shohin;
+-- DISTINCTを使うと重複行を省く
+SELECT DISTINCT shohin_bunrui FROM shohin;
+-- NULLを含むDISTINCTをつける
+SELECT DISTINCT shiire_tanka FROM shohin;
+-- DISTINCTキーワードは先頭の列名の前にしか書けない
+SELECT DISTINCT shohin_bunrui, torokubi FROM shohin;
