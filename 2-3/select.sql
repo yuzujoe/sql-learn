d1de0bb519eae7fb910c64f6d0455b0101f14a62
yuzujoe <yuzun225@gmail.com>
2020-02-17 09:19:37 +0900

    () operator

--- 2-3/select.sql
+++ 2-3/select.sql
@@ -2,5 +2,7 @@
 SELECT shohin_mei, shohin_bunrui, hanbai_tanka FROM shohin WHERE NOT hanbai_tanka >= 1000;
 -- andでwhereの両辺の条件にマッチするものを出力
 SELECT shohin_mei, shiire_tanka FROM shohin WHERE shohin_bunrui = 'キッチン用品' and hanbai_tanka >= 3000;
--- どちらかが成り立つのであればor
+-- どちらかが成り立つのであればor orよりもandが優先される
 SELECT shohin_mei, shiire_tanka FROM shohin WHERE shohin_bunrui = 'キッチン用品' OR hanbai_tanka >= 3000;
+-- ()で囲むことによってORを優先させる
+SELECT shohin_mei, shohin_bunrui, torokubi FROM shohin WHERE shohin_bunrui = '事務用品' AND ( torokubi = '2009-09-11' or torokubi = '2009-09-20' )
