91132ce8de257e9e448bd085890f7c38b0275bba
yuzujoe <yuzun225@gmail.com>
2020-03-16 00:26:15 +0900

    practice part2

--- 2-3/practice.sql
+++ 2-3/practice.sql
@@ -1,2 +1,17 @@
 -- 2.1
-SELECT shohin_mei, torokubi FROM shohin WHERE torokubi >= 2009-04-28
+SELECT shohin_mei, torokubi FROM shohin WHERE torokubi >= 2009-04-28;
+
+-- 2.3
+SELECT shohin_mei, hanbai_tanka, shiire_tanka 
+  FROM shohin 
+  WHERE hanbai_tanka - 500 >= shiire_tanka;
+
+SELECT shohin_mei, hanbai_tanka, shiire_tanka
+  FROM shohin
+  WHERE hanbai_tanka  >= shiire_tanka + 500;
+
+-- 2.4
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka * 0.9 - shiire_tanka AS rieki
+  FROM shohin
+  WHERE hanbai_tanka * 0.9 - shiire_tanka > 100 
+    AND (shohin_bunrui = '事務用品' OR shohin_bunrui = 'キッチン用品');
