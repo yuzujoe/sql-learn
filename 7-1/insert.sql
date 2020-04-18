4e1b5965313e3e21697b7a84cdb6e74773099cdb
yuzujoe <yuzun225@gmail.com>
2020-04-18 09:13:01 +0900

    create table and insert data

---
+++ 7-1/insert.sql
@@ -0,0 +1,9 @@
+START TRANSACTION;
+
+INSERT INTO shohin2 VALUES ('1', 'Tシャツ', '衣類', 1000, 500, '2009-09-20');
+INSERT INTO shohin2 VALUES ('2', '穴あけパンチ', '事務用品', 500, 320, '2009-09-20');
+INSERT INTO shohin2 VALUES ('3', 'カッターシャツ', '衣服', 4000, 2800, '2009-09-20');
+INSERT INTO shohin2 VALUES ('9', '手袋', '衣類', 800, 500, null);
+INSERT INTO shohin2 VALUES ('10', 'やかん', 'キッチン用品', 2000, 1700, '2009-09-20');
+
+COMMIT;
