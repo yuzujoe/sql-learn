10b7f8500a5c2d3edc39cde1eccd5054536544e7
yuzujoe <yuzun225@gmail.com>
2020-04-19 09:24:32 +0900

    Window function to find the average value

--- 8-1/rank.sql
+++ 8-1/rank.sql
@@ -8,3 +8,7 @@ select shohin_mei, shohin_bunrui, hanbai_tanka,
     DENSE_RANK () over (order by hanbai_tanka) as  dense_ranking,
     ROW_NUMBER () over (order by hanbai_tanka) as  row_num
     from shohin;
+
+select shohin_id, shohin_mei, hanbai_tanka,
+    sum (hanbai_tanka) over (order by shohin_id) as current_sum
+    from shohin;
