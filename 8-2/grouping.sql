4fe508dd661aead0d2ddfeeed5d21a3d51c6cb5c
yuzujoe <yuzun225@gmail.com>
2020-04-19 10:06:19 +0900

    grouping

--- 8-2/grouping.sql
+++ 8-2/grouping.sql
@@ -5,3 +5,17 @@ select shohin_bunrui, sum(hanbai_tanka) as sum_tanka
 select shohin_bunrui, torokubi, sum(hanbai_tanka) as sum_tanka
     from shohin
     group by shohin_bunrui, torokubi with rollup;
+
+select grouping(shohin_bunrui) as shohin_bunrui,
+        grouping(torokubi) as torokubi, sum(hanbai_tanka) as sum_tanka
+    from shohin
+    group by shohin_bunrui, torokubi with rollup;
+
+select  case  when grouping(shohin_bunrui) = 1
+              then '商品分類 合計'
+              else shohin_bunrui end as shohin_bunrui,
+        case  when grouping(torokubi) = 1
+              then '登録日　合計'
+              else cast(torokubi as varchar(16)) end as torokubi,
+    from shohin;
+    group by shohin_bunrui, torokubi with rollup;
