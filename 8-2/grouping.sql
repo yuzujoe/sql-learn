5cbe0d19c7935d86aca3c0caa1c1ec6d1bf522e0
yuzujoe <yuzun225@gmail.com>
2020-04-19 10:09:39 +0900

    cube

--- 8-2/grouping.sql
+++ 8-2/grouping.sql
@@ -19,3 +19,12 @@ select  case  when grouping(shohin_bunrui) = 1
               else cast(torokubi as varchar(16)) end as torokubi,
     from shohin;
     group by shohin_bunrui, torokubi with rollup;
+
+select  case  when grouping(shohin_bunrui) = 1
+              then '商品分類 合計'
+              else shohin_bunrui end as shohin_bunrui,
+        case  when grouping(torokubi) = 1
+              then '登録日　合計'
+              else cast(torokubi as varchar(16)) end as torokubi,
+    from shohin;
+    group by cube(shohin_bunrui, torokubi);
